19f57d8fcbb0dfabf83da56e5867da640f813031
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-19 12:23:10 -0500

    stock model tests for creating and persisting to db

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,5 @@
 class Stock < ActiveRecord::Base
+  # validate stock symbol and name are in the db and that only one instance is saved
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :ticker, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should save stock symbol and name" do
+    @stock = Stock.create!(ticker: 'MSFT', name: 'Micorsoft Corporation')
+    expect(@stock.ticker).to eq('MSFT')
+    expect(@stock.name).to eq('Micorsoft Corporation')
+  end
+
+  it "should not persist to the database" do
+    @stock = Stock.new(ticker: 'MSFT')
+    expect(@stock.save).to eq(false)
+  end
 end
