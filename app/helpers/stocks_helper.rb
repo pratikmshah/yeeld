148bb84f931fc195f323c55c674f5e5352024d8a
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-20 14:07:17 -0500

    add method to stock helper to calculate real time market value

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -9,7 +9,7 @@ module StocksHelper
   def info_request
     [:name, :symbol, :ask, :change_and_percent_change, :last_trade_date, :days_range,
      :weeks_range_52, :open, :volume, :average_daily_volume, :market_capitalization,
-     :pe_ratio, :dividend_per_share, :dividend_yield, :earnings_per_share, :float_shares, :ebitda]
+     :pe_ratio, :dividend_per_share, :dividend_yield, :earnings_per_share, :float_shares, :ebitda, :change, :close, :previous_close]
   end
 
   # returns historical stock data for over a year
@@ -23,6 +23,23 @@ module StocksHelper
     dates = parse_date(data)
     prices = parse_price(data)
     parse_chart_data(dates, prices)
+  end
+
+  # get market price
+  def get_market_price(stock)
+
+    # get previous close value, convert to float and round to 2 decimals
+    open = stock.previous_close.to_f.round(2)
+
+    # get change value of stock, convert to array and then to float to 2 decimals
+    change_val = stock.change.split().first[1..-1].to_f.round(2)
+
+    #depending on if stock is up or down return price
+    if stock.change[0] == '+'
+      return (open + change_val)
+    else
+      return (open - change_val)
+    end
   end
 
   # below methods to be used as helpers for the above methods
