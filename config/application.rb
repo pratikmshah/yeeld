cd9384acc7c4282ac4941d28a39d4461c90a7002
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-23 18:11:11 -0500

    add assets compile

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Yeeld
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_assets = true
+    config.assets.compile = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
