f99f2f38fd69c2b3b0d30343cafaa5958cb17144
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-24 11:45:31 -0500

    prevent user from submitting empty stock form

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+
+  // set submit button to be disabled initialially
+  var $stock_add_btn = $("#add-stock input:last");
+  var $add_stock_val = '';
+  $stock_add_btn.attr('disabled', true);
+
+  // on keystroke up check to see if there is value in the box
+  // if true then take disabled off button else keep disabled
+  $("#add-stock").on('keyup', function(){
+    $add_stock_val = $('#add-stock input').val();  // grab value of input box
+
+    if ($add_stock_val.length) {
+      $stock_add_btn.attr('disabled', false);
+    } else {
+      $stock_add_btn.attr('disabled', true);
+    }
+
+  });
+
+});
