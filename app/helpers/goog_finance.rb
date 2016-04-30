d92f28a2dc71cb1727552b370da3f6530aec3560
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-30 00:18:28 -0500

    parse company summary and reuters link

--- app/helpers/goog_finance.rb
+++ app/helpers/goog_finance.rb
@@ -1,8 +1,13 @@
 module GoogFinance
   QUOTE_URL = 'https://www.google.com/finance?q='
   COMPANY_DESC_SELECTOR = 'div.companySummary'
+  REUTER_LINK = 'a#m-rprofile'
 
   def company_summary(symbol)
-    data = Nokogiri::HTML(open("#{QUOTE_URL}#{symbol.upcase}")).css(COMPANY_DESC_SELECTOR)
+    data = { summary: '', rlink: '' }
+    html = Nokogiri::HTML(open("#{QUOTE_URL}#{symbol.upcase}"))
+    data[:summary] = html.css(COMPANY_DESC_SELECTOR).first.text
+    data[:rlink] = html.css(REUTER_LINK).first.attr('href')
+    return data
   end
 end
