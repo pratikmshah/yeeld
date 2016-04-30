6e73ecdd450f63a763a49b6a64a73a47af930b2d
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-30 13:40:41 -0500

    parse excerpt from article

--- app/helpers/goog_finance.rb
+++ app/helpers/goog_finance.rb
@@ -2,13 +2,14 @@ module GoogFinance
 
   # Company description
   QUOTE_URL = 'https://www.google.com/finance?q='
-  COMPANY_DESC_SELECTOR = 'div.companySummary'                  # returns the company description
-  REUTER_LINK = 'a#m-rprofile'                                  # returns url link to reuters company profile
+  COMPANY_DESC_SELECTOR = 'div.companySummary'                    # returns the company description
+  REUTER_LINK = 'a#m-rprofile'                                    # returns url link to reuters company profile
 
   # Company news articles
   NEWS_URL = 'https://www.google.com/finance/company_news?q='
-  NEWS_LINKS_SELECTOR = 'div#news-main div.news span.name a'    # returns the title and url link of article
-  NEWS_SRC_SELECTOR   = 'div#news-main div.news div.byline'     # returns src of article ex: Daily Mail - Apr 15, 2015
+  NEWS_LINKS_SELECTOR = 'div#news-main div.news span.name a'      # returns the title and url link of article
+  NEWS_SRC_SELECTOR   = 'div#news-main div.news div.byline'       # returns src of article ex: Daily Mail - Apr 15, 2015
+  NEWS_EXCERPT_SELECTOR   = 'div#news-main div.news div.g-c div'  # returns src of article ex: Daily Mail - Apr 15, 2015
 
   def company_summary(symbol)
     data = { summary: '', rlink: '' }
@@ -21,15 +22,11 @@ module GoogFinance
   def company_news(symbol)
     data = []
     html = Nokogiri::HTML(open("#{NEWS_URL}#{symbol.upcase}"))
-    data.push(info_to_array(html.css(NEWS_LINKS_SELECTOR)))                   # push headline to array
-    data.push(parse_urls(html.css(NEWS_LINKS_SELECTOR)))                      # push url links to array
-    data.push(info_to_array(html.css(NEWS_SRC_SELECTOR)))                     # push source text to array
-    return data
-  end
-
-  def test(symbol)
-    html = Nokogiri::HTML(open("#{NEWS_URL}#{symbol.upcase}"))
-    parse_urls(html.css(NEWS_LINKS_SELECTOR))
+    data.push(info_to_array(html.css(NEWS_LINKS_SELECTOR)))       # push headline to array
+    data.push(parse_urls(html.css(NEWS_LINKS_SELECTOR)))          # push url links to array
+    data.push(info_to_array(html.css(NEWS_SRC_SELECTOR)))         # push source text to array
+    data.push(info_to_array(html.css(NEWS_EXCERPT_SELECTOR)))     # push news excerpt text to array
+    return data.transpose
   end
 
   private
