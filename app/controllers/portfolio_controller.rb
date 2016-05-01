deff791e7157db37e821bdd6c8df0eb532234362
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-30 20:03:09 -0500

    sort stocks by ticker symbol

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,8 +2,8 @@ class PortfolioController < ApplicationController
 
   def index
     @stock = Stock.new
-    @user_portfolio = current_user.stocks               # get list of all user's stock porfolio
-    @portfolio = Stock.stock_tickers(@user_portfolio)   # retrieve only the stock tickers from the list
+    @user_portfolio = current_user.stocks                    # get list of all user's stock porfolio
+    @portfolio = Stock.stock_tickers(@user_portfolio).sort   # retrieve only the stock tickers from the list and sort by ticker symbol
   end
 
   def show
