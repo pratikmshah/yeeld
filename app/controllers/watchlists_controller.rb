839830985874e0f6dcb27c5b1ec2ba77519a624f
Pratik Shah <pratik.t.shah@gmail.com>
2016-05-04 17:01:50 -0500

    add watchlist controller for create

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -3,22 +3,46 @@ class WatchlistsController < ApplicationController
   def create
     user = current_user
 
+    # check to see if stock is in db
+    watch = Watchlist.find_by_ticker(watchlist_param[:ticker].upcase)
+    if watch
 
+      # check to see if user already has in portfolio
+      if !stock_in_portfolio?(user, watch)
+        @add_stock_watchlist = UserWatchlist.new(user_id: user.id, watchlist_id: watch.id)   # create new record in join table
+        @add_stock_watchlist.save
+      end
 
-    respond_to do |format|
-     format.html { redirect_to portfolio_index_path }
-     format.js
+    else
+
+      # if not check api to get values and then save stock
+      watch = Watchlist.new_from_lookup(watchlist_param[:ticker])
+      if watch
+        watch.save
+
+        # check to see if user already has in portfolio
+        if !stock_in_portfolio?(user, watch)
+          @add_stock_watchlist = UserWatchlist.new(user_id: user.id, watchlist_id: watch.id)   # create new record in join table
+          @add_stock_watchlist.save
+        end
+
+      else
+        # if not in api return back with error msg
+        flash[:notice] = "Symbol was not found."
+      end
     end
+
+    redirect_to portfolio_index_path
   end
 
   private
 
-    def stock_param
-      params.require(:stock).permit(:ticker)
+    def watchlist_param
+      params.require(:watchlist).permit(:ticker)
     end
 
-    def stock_in_portfolio?(user, stock)
-      UserStock.where(user_id: user.id, stock_id: stock.id).exists?
+    def stock_in_portfolio?(user, watch)
+      UserWatchlist.where(user_id: user.id, watchlist_id: watch.id).exists?
     end
 
 end
--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -9,4 +9,34 @@ class Watchlist < ActiveRecord::Base
   # before saving uppercase all letters
   before_save :uppercase_field
 
+
+  # return ticker symbol in our database instead of querying
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+  # lookup ticker through stock quote
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    return nil unless looked_up_stock.name
+
+    new_watch = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+    new_watch
+  end
+
+  # return current ask price of stock
+  def self.price(ticker_symbol)
+    StockQuote::Stock.quote(ticker_symbol).ask
+  end
+
+  # parse through array of stock objects returning ticker in new array
+  def self.stock_tickers(stock_obj)
+    stock_obj.map { |stock| stock.ticker }
+  end
+
+  private
+
+    def uppercase_field
+      self.ticker.upcase!
+    end
 end
